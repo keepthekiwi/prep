6fcbdd60b5ecbefe01707355a3ae3f26ab4ee5c1
skillian <90988162+keepthekiwi@users.noreply.github.com>
2022-04-19 11:10:19 +0200

    .

--- Eigener Spielplatz/M04_pgcd/try_out_area.c
+++ Eigener Spielplatz/M04_pgcd/try_out_area.c
@@ -12,18 +12,18 @@ void	pgcd(int a, int b)						//av1 und av2
 	int i;
 	int gcd;
 
-	i = 1;										// 1 das ist der Nenner mit dem ich es versuche// und dann gehe ich immer hoeher
-	gcd = 0;									// 0 //muesste das nicht 1 sein, da durch 0 kann man ja nicht teilen
+	i = 1;									// 1 das ist der Nenner mit dem ich es 													//versuche// und dann gehe ich immer hoeher
+	gcd = 0;									// 0 //muesste das nicht 1 sein, da durch 0 												//kann man ja nicht teilen
 	while (i < a && i < b)
 	{
-		if (a % i == 0 && b % i == 0)			// so lange bist rest ensteht, i immer 1 hoeher
+		if (a % i == 0 && b % i == 0)		// so lange bist rest ensteht, i immer 1 hoeher
 			gcd = i; //!!
 		i++;
 	}
 	printf("%d", gcd);
 }
 
-int		main(int ac, char **av)					// int, char
+int		main(int ac, char **av)				// int, char
 {
 	if (ac == 3)
 		pgcd(atoi(av[1]), atoi(av[2]));
